e0a27b4fa40f9d017fe71f0465ef346d7bbedb7b
Lucas Cheung <Lucas_z@greypanel.com>
2023-03-08 17:38:36 +0800

    first update with basic demo features

---
+++ routers/items.py
@@ -0,0 +1,28 @@
+import datetime
+import time
+from typing import List
+
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from fastapi import Depends, APIRouter, HTTPException
+from sqlalchemy.orm import Session
+
+from db import schemas, crud
+from db.database import get_db
+from dependencies import templates
+from logs.logger import logger
+
+router = APIRouter()
+
+
+@router.get("/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("item_view.html", {"request": request, "id": id})
+
+
+
+@router.get("/", response_model=List[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    # return items
+    return templates.TemplateResponse("items.html",{"my_list" : items})
