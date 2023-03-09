cb5d0f70894d0a0d4918a1fa4e551dd900a5c991
Lucas Cheung <Lucas_z@greypanel.com>
2023-03-09 18:07:59 +0800

    first update with basic demo features

---
+++ lib/utilsJson.py
@@ -0,0 +1,37 @@
+import json
+
+from fastapi import Depends
+
+from db import crud
+from db.database import get_db
+
+
+def dict_to_json(dict_input):
+    json_output = json.dumps(dict_input, indent=4)
+    return json_output
+
+
+def json_to_dict(json_input):
+    output_dict = json.loads(json_input)
+    return output_dict
+
+def objetc_to_json(object_input):
+    # 使用 dumps函数直接将对象转化为JSON结果
+    # ensure_ascii 默认为True，会将汉字转换为ascii码
+    result = json.dumps(object_input,ensure_ascii = False)
+    return result
+
+if __name__ == '__main__':
+    dict_input = {"name": "zhangsan", "age": 18}
+    json_output = dict_to_json(dict_input)
+    print("字典转json的结果: ", json_output)
+
+    json_input = '{"name": "zhangsan", "age": 18}'
+    output_dict = json_to_dict(json_input)
+    print("json转字典的结果: ", output_dict)
+
+    object_input = {"name": "zhangsan", "age": 18}
+    result = objetc_to_json(object_input)
+    print("对象转json的结果: ", result)
+
+
--- routers/items.py
+++ routers/items.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 import time
 from typing import List
 from fastapi.params import Form
@@ -6,16 +7,28 @@ from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi import Depends, APIRouter, HTTPException
 from sqlalchemy.orm import Session
+from starlette import status
+from starlette.responses import RedirectResponse, FileResponse
+from starlette.background import BackgroundTask
 
-from db import schemas, crud
+from db import schemas, crud, models
 from db.database import get_db
+from db.schemas import ItemBase
 from dependencies import templates
+from lib.utilsJson import objetc_to_json
 from logs.logger import logger
 
 router = APIRouter()
 
+@router.get("/", response_model=List[schemas.Item])
+def read_items(request: Request, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    # return items
+    new_item = schemas.ItemCreate
+    return templates.TemplateResponse("items.html",{"request": request, "item_list": items, "new_item": new_item})
 
-@router.get("/{id}", response_class=HTMLResponse)
+
+@router.get("/id={id}", response_class=HTMLResponse)
 async def read_item(request: Request, id: str):
     return templates.TemplateResponse("item_view.html", {"request": request, "id": id})
 
@@ -32,12 +45,27 @@ async def create_item(
             description=set_description
         )
     crud.create_user_item(db=db, item=new_item, user_id=set_user_id)
-    request = Request
-    return read_items(request=request, skip=0, limit=100, db=db)
 
-@router.get("/", response_model=List[schemas.Item])
+    response = RedirectResponse('/items', status_code=status.HTTP_302_FOUND)
+    return response
+
+@router.get("/download", response_model=List[schemas.Item])
 def read_items(request: Request, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
-    # return items
-    new_item = schemas.ItemCreate
-    return templates.TemplateResponse("items.html",{"request": request, "item_list": items, "new_item": new_item})
+
+    item_list = []
+    for item in items:
+        print(item.__dict__)
+
+        item_list.append(item.__dict__)
+    # file_content = objetc_to_json(items)
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    rootPath = basedir[:basedir.find('demo_fastapi')+len('demo_fastapi')]
+
+    file_path = os.path.join(rootPath, 'temp', 'test.txt')
+    print(file_path)
+    with open(file_path, 'w') as f:
+        f.write(str(item_list))
+    return FileResponse(file_path, media_type='text/plain', filename='test.txt',background=BackgroundTask(lambda: os.remove(file_path)))
+
+
