8d50524650e9ef15e02c5898ec9ce5e96ebd9b2a
Lucas Cheung <Lucas_z@greypanel.com>
2023-03-10 18:02:30 +0800

    update with new framework

--- routers/login.py
+++ routers/login.py
@@ -1,18 +1,21 @@
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi import Depends,APIRouter
+from jose import jwt, JWTError
 from sqlalchemy.orm import Session
 from datetime import timedelta
 from fastapi import status,HTTPException
 
-from db.crud.login import authenticate_user
+from db.crud.login import authenticate_user, get_user
 from db.database import get_db
 from core.hashing import Hasher
 from core.security import create_access_token
 from core.config import settings
 from db.schemas.tokens import Token
+from fastapi.security import OAuth2PasswordRequestForm,OAuth2PasswordBearer
 
 router = APIRouter()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")  #new
 
 @router.post("/token", response_model=Token)
 def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(),db: Session= Depends(get_db)):
@@ -28,3 +31,21 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(),db: 
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
+#new function, It works as a dependency
+def get_current_user_from_token(token: str = Depends(oauth2_scheme),db: Session=Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        username: str = payload.get("sub")
+        print("username/email extracted is ",username)
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = get_user(username=username,db=db)
+    if user is None:
+        raise credentials_exception
+    return user
